8b5cd37bb3366c4ad9eec09fab0d0bf6337e826c
Marmaduke <jmeppley@gmail.com>
2017-08-03 15:56:49 -0700

    issue #1: added code to parse out rule names and do two things: remove them from the workflow and pass them as forcerun targets when executing

--- prototype/snakemake_magic.py
+++ prototype/snakemake_magic.py
@@ -1,9 +1,14 @@
 """
 Defines magic functions to run snakemake from within ipython:
 
+The current approach is to create a snakemake.workflow object the first time a cell
+magic is called. The contents of the cell are put into a temporary file and loaded
+into the workflow by using worfklow.include() or workflow.config.update().
+
 """
 from __future__ import print_function
 import os
+import re
 import tempfile
 import shlex
 from IPython.core.magic import (Magics, magics_class, line_magic,
@@ -12,7 +17,19 @@ from IPython.core.magic import (Magics, magics_class, line_magic,
 from snakemake.workflow import Workflow
 import snakemake.workflow
 from snakemake.io import load_configfile, _load_configfile
+from snakemake.parser import parse
 from snakemake import get_argument_parser, parse_resources, logger
+
+rule_rexp = re.compile(r'^\s*@workflow.rule\s*\(name=\s*\'([^\']+)\'')
+def get_rule_names(snakefile_name):
+    """ Run snakemake.parser.parse and use regexp to get the rule names
+        This is incredibly inefficient, but a little more stable than trying to
+        parse rules myself.
+    """
+    for line in parse(snakefile_name)[0].split("\n"):
+        m = rule_rexp.match(line)
+        if m:
+            yield m.group(1)
 
 # To register magic, class MUST call this class decorator at creation time
 @magics_class
@@ -26,6 +43,7 @@ class SnakemakeMagic(Magics):
     """
     workflow = None
     tempfiles = {"cells":[]}
+    updated_rules = []
 
     # TODO: add magic to reset workflow with more options
     #  EG: cluster script, threads, ...
@@ -35,27 +53,32 @@ class SnakemakeMagic(Magics):
 
         TODO:
             * allow multiple workflows?
+            * what king of options to allow?
             * allow options every time or just first?
         """
         if self.workflow is None:
             # create a new workflow object with some basic defaults
 
-            # this file cannot be read from on some Windows systems...
+            # create a blank file just so snakemake has something to hang on to
+            # (this file cannot be read from on some Windows systems...)
             self.tempfiles['root'] = tempfile.NamedTemporaryFile('w')
             self.workflow = Workflow(snakefile=self.tempfiles['root'].name)
 
         return self.workflow
-
+    
     @line_magic
     def snakemake(self, line):
-        "execute the workflow with the given arguments and targets"
+        """ execute the workflow with the given arguments and targets
+        
+        This uses the snakeamke command line argument parser for now.
+        """
         if self.workflow is None:
             raise Exception("Workflow has no data!")
 
         parser = get_argument_parser()
         args = parser.parse_args(list(shlex.split(line)))
 
-        print(repr(args))
+        logger.debug(repr(args))
         
         resources = parse_resources(args)
 
@@ -67,7 +90,7 @@ class SnakemakeMagic(Magics):
         printd3dag = args.d3dag
         touch = args.touch
         forceall = args.forceall
-        forcerun = args.forcerun
+        forcerun = set(args.forcerun if args.forcerun is not None else [] + self.updated_rules)
         prioritytargets = args.prioritize
         until = args.until
         omit_from = args.omit_from
@@ -110,6 +133,9 @@ class SnakemakeMagic(Magics):
         workflow = self.get_workflow()
         # TODO: keep track of updated rules to set force run
 
+        # HACK: execute() is leaving directory locked, so I'm disabling locks
+        lock = False
+        
         workflow.check()
 
         success = workflow.execute(
@@ -147,6 +173,10 @@ class SnakemakeMagic(Magics):
             updated_files=updated_files,
             resources=resources,
             )
+        
+        if success:
+            del self.updated_rules[:]
+        
         return success
 
     @cell_magic
@@ -161,30 +191,48 @@ class SnakemakeMagic(Magics):
         self.tempfiles['cells'].append(cell_snakefile.name)
         cell_snakefile.write(cell)
         cell_snakefile.close()
+        
         # first rule is first rule
         overwrite_first_rule = len(workflow._rules) == 0
+
+        # HACK: remove rules to be replaced:
+        # remove conflicting rules
+        for rule_name in get_rule_names(cell_snakefile.name):
+            workflow._rules.pop(rule_name, None)
+            self.updated_rules.append(rule_name)
+
+        # include snippet
         workflow.include(cell_snakefile.name,
                          overwrite_first_rule=overwrite_first_rule,
                         )
         os.unlink(cell_snakefile.name)
 
-        print(repr(workflow._rules))
         return "Workflow now has {} rules".format(len(workflow._rules))
 
     @cell_magic
     def sconfig(self, line, cell):
         " Load JSON or YAML into workflow's config object "
         workflow = self.get_workflow()
+
+        # create a temp file, so we can use snakemake.load_configfile
+        #  it wouldn't be hard to roll our own to avoid this...
         cell_config_file = tempfile.NamedTemporaryFile('w', delete=False)
         cell_config_file.write(cell)
         cell_config_file.close()
+        
         snakemake.workflow.config.update(load_configfile(cell_config_file.name))
-        print(repr(snakemake.workflow.config))
+        logger.debug(repr(snakemake.workflow.config))
         os.unlink(cell_config_file.name)
 
     @line_magic
     def _workflow(self, line):
+        "backdoor to inspect workflow object"
+        # this wouldn't be in the final version
         return self.get_workflow()
+    
+    @line_magic
+    def _reset_workflow(self, line):
+        self.workflow = None
 
 # In order to actually use these magics, you must register them with a
 # running IPython.  This code must be placed in a file that is loaded once
